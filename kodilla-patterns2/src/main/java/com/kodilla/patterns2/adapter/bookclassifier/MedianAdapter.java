a0618ac93faef97619308f95743adce89db847c4
Grzegorz Wrobel <grzegorz.j.wrobel@gmail.com>
2022-06-18 22:38:11 +0200

    24.5 - Design pattern 2 - Adapter-fixed class name conflict

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapter.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapter.java
@@ -1,18 +1,16 @@
 package com.kodilla.patterns2.adapter.bookclassifier;
 
-import com.kodilla.patterns2.adapter.bookclassifier.librarya.Book;
 import com.kodilla.patterns2.adapter.bookclassifier.librarya.Classifier;
 import com.kodilla.patterns2.adapter.bookclassifier.libraryb.BookSignature;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class MedianAdapter extends MedianAdaptee implements Classifier {
 
     @Override
-    public int publicationYearMedian(Set<Book> bookSet) {
+    public int publicationYearMedian(Set<com.kodilla.patterns2.adapter.bookclassifier.librarya.Book> bookSet) {
         Map<BookSignature, com.kodilla.patterns2.adapter.bookclassifier.libraryb.Book> books = new HashMap<>();
-        for (Book theBook : bookSet) {
+        for (com.kodilla.patterns2.adapter.bookclassifier.librarya.Book theBook : bookSet) {
             books.put(new BookSignature(theBook.getSignature()),
                     new com.kodilla.patterns2.adapter.bookclassifier.libraryb.Book(
                             theBook.getAuthor(),
